174c686277e5efc52477d2175aa649f81da43c15
pandurijal <pandurijal96@gmail.com>
2024-04-29 22:10:41 +0700

    initial app

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,33 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title: "Performance | Overview",
+  description: "",
+  // other metadata
+};
+
+const Performance = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Performance" />
+
+      <div className="rounded-sm border border-stroke bg-white p-1 shadow-default dark:border-strokedark dark:bg-boxdark md:p-6 xl:p-9">
+        <div className="flex flex-col gap-7.5 overflow-hidden">
+          <iframe
+            width="1200"
+            height="1800"
+            src="https://lookerstudio.google.com/embed/reporting/29a6e502-218c-449a-b80d-d39c8ca1f1c4/page/p_yqqsaioecd"
+            frameBorder={"0"}
+            style={{ border: 0 }}
+            allowFullScreen
+            sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+          />
+        </div>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Performance;
